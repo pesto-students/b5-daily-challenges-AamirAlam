2f8a2d933020f6023af59c2785c3ffdf1136caf2
= <=>
2021-01-06 17:56:58 +0530

    Fix edge cases

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,14 +1,19 @@
 // import { isString } from "util";
 
 function abbreviateString(str) {
-    if (typeof str !== 'string'){
-        throw 'Invalid argument! it must be a string';
-    }
-    const allWords = str.split(' ')
-    const firstWord = allWords[0];
-    const lastWord = allWords[allWords.length-1];
 
-    return firstWord+" "+lastWord[0].toUpperCase()+"."
+    if (typeof str !== 'string'){
+        throw new TypeError('Invalid argument! it must be a string');
+    }
+    
+    str = str.trim()
+    if (str.indexOf(' ') < 0 ) {
+        return str;    
+    }else{
+        const firstWord = str.slice(0,str.indexOf(' '))
+        const lastWord =  str.slice(str.lastIndexOf(' ')+1, str.length )
+        return  firstWord+" "+lastWord[0].toUpperCase()+"."
+    }
 }
 
 export { abbreviateString };
